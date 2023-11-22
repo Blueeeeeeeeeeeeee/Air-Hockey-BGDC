a3588596262192686c39830fe76628980150fcfa
l8mego <jhongsonkho@gmail.com>
2023-11-22 15:23:52 +0700

    Session5Stuff

--- Assets/Scenes/Session4CameraAndCollision/Circle.cs
+++ Assets/Scenes/Session4CameraAndCollision/Circle.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
 
+
+
 public class Session4 : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector2 movement;
     public float speed=5f;
     public Rigidbody2D B;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
-    // Update is called once per frame
     void Update()
     {
         movement.x=Input.GetAxisRaw("Horizontal");
         movement.y=Input.GetAxisRaw("Vertical");
+
+        if(Input.GetAxisRaw("Vertical")>0f||Input.GetAxisRaw("Horizontal")>0f){
+            animator.SetBool("Hitted",true);
+        }
+        else{
+            animator.SetBool("Hitted",false);
+        }
+        
     }
 
     private void FixedUpdate() {
         B.MovePosition(B.position+movement.normalized*speed);
     }
+    // private void OnCollisionEnter2D(Collision2D other) {
+    //     if(other.collider.tag=="Enemy"){
+    //         animator.SetBool("Hitted",true);
+    //     }
+    // }
+    // private void OnCollisionExit2D(Collision2D other) {
+    //     if(other.collider.tag=="Enemy"){
+    //         animator.SetBool("Hitted",false);
+    //     }
+    // }
 
 }
--- Assets/Scripts/hitchecker.cs
+++ Assets/Scripts/hitchecker.cs
@@ -6,11 +6,7 @@ public class hitchecker : MonoBehaviour
 {
 
     public Animator animator;
-    /// <summary>
-    /// Sent when an incoming collider makes contact with this object's
-    /// collider (2D physics only).
-    /// </summary>
-    /// <param name="other">The Collision2D data associated with this collision.</param>
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.collider.tag!="Puck"){
@@ -20,11 +16,7 @@ public class hitchecker : MonoBehaviour
 
 
     }
-    /// <summary>
-    /// Sent when a collider on another object stops touching this
-    /// object's collider (2D physics only).
-    /// </summary>
-    /// <param name="other">The Collision2D data associated with this collision.</param>
+
     void OnCollisionExit2D(Collision2D other)
     {
         if(other.collider.tag!="Puck"){
